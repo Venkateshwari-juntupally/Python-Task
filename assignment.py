c87b81180104e0b7eb26ffe2e23f3d35344c8171
Venkateshwari-juntupally <124126288+Venkateshwari-juntupally@users.noreply.github.com>
2023-06-27 16:24:58 +0530

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,91 +1,80 @@
-import csv
-import datetime
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.utils import COMMASPACE
-from os.path import basename
-
+import whois
 import pandas as pd
 import schedule
-import whois
+import time
+import smtplib
+import logging
 
+# Configure logging
+logging.basicConfig(filename='error.log', level=logging.ERROR, format='%(asctime)s %(levelname)s: %(message)s')
 
-def get_whois_data(domains):
-    received_data = []
-    for section in domains:
+def extract_whois_details():
+    domains = ['google.com', 'amazon.com', 'facebook.com']
+    
+    data = pd.DataFrame(columns=['Date', 'Time', 'Domain', 'Name', 'Email', 'Phone'])
+
+    for domain in domains:
         try:
-            w = whois.whois(section)
+            w = whois.whois(domain)
+
+            # Extract the required details
             name = w.name
             email = w.email
             phone = w.phone
-            date_of_registration= w.creation_date
-            received_data.append([name,section, email, phone, date_of_registration])
+
+            now = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+
+            # Add the details to the DataFrame
+            data = data.append({'Date': now.split()[0],
+                                'Time': now.split()[1],
+                                'Domain': domain,
+                                'Name': name,
+                                'Email': email,
+                                'Phone': phone}, ignore_index=True)
         except Exception as e:
-            log_error(f"Error extracting WHOIS data for {section}: {str(e)}")
-    return received_data
+            logging.error(f'Error extracting WHOIS details for {domain}: {str(e)}')
 
+    # Perform deduplication and filtering if needed
+    data.drop_duplicates(inplace=True)  # Remove duplicate entries
+    data = data[data['Name'].notna()]  # Filter out entries without a name
 
-def store_data_in_csv (data):
-    filename = f"whois_data_{datetime.datetime.now().strftime('%Y%m%d')}.csv"
-    with open(filename, "a", newline="") as file:
-        writer = csv.writer(file)
-        writer.writerows(data)
-    print(f"Extracted data stored in {filename}")
+    # Save the extracted details to a database or CSV file
+    data.to_csv('whois_details.csv', index=False)
 
+    # Send an email with the extracted details
+    send_email(data)
 
-def send_email (data):
-    sender_email = "your_email@example.com"
-    sender_password = "your_email_password"
-    recipient_email = "recipient_email@example.com"
+# Function to send an email with the extracted details
+def send_email(data):
+    sender_email = 'u7569543@gmail.com'
+    receiver_email = 'jvenkateshwari.99@gmail.com'
+    smtp_server = 'smtp.gmail.com'
+    smtp_port = 587
+    smtp_username = 'u7569543@gmail.com'
+    smtp_password = 'User@123'
 
-    subject = f"WHOIS Data -data {datetime.datetime.now().strftime('%Y-%m-%d')}"
-    body = "Please find attached the WHOIS data for newly registered domains."
-    attachment = f"whois_data_{datetime.datetime.now().strftime('%Y%m%d')}.csv"
-
-    message = MIMEMultipart()
-    message["From"] = sender_email
-    message["To"] = COMMASPACE.join([recipient_email])
-    message["Subject"] = subject
-
-    message.attach(MIMEText(body, "plain"))
-
-    with open(attachment, "rb") as file:
-        part = MIMEText(file.read(), "csv")
-        part.add_header("Content-Disposition", f"attachment; filename={basename(attachment)}")
-        message.attach(part)
+    # Compose the email message
+    subject = 'WHOIS Details for Newly Registered Domains'
+    body = data.to_string(index=False)
+    message = f'Subject: {subject}\n\n{body}'
 
     try:
-        server = smtplib.SMTP("smtp.gmail.com", 587)
+        # Connect to the SMTP server and send the email
+        server = smtplib.SMTP(smtp_server, smtp_port)
         server.starttls()
-        server.login(sender_email, sender_password)
-        server.send_message(message)
+        server.login(smtp_username, smtp_password)
+        server.sendmail(sender_email, receiver_email, message)
         server.quit()
-        print("Email sent successfully.")
     except Exception as e:
-        log_error(f"Error sending email: {str(e)}")
+        logging.error(f'Error sending email: {str(e)}')
 
+# Schedule the script to run daily at a specified time (e.g., 9:00 AM)
+schedule.every().day.at('09:00').do(extract_whois_details)
 
-def log_error(error_message):
-    with open("error.log", "a") as file:
-        timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        file.write(f"{timestamp} - {error_message}\n")
-
-
-def run_script():
-    # List of newly registered domains
-    domains = ["example1.com", "example2.com", "example3.com"]
-
-    # Extract WHOIS data
-    received_data = get_whois_data(domains)
-
-    # Store data in CSV
-    store_data_in_csv(received_data)
-
-    # Send email with data
-    send_email(received_data)
-# Schedule the script to run daily at a specified time
-schedule.every().day.at("10:00").do(run_script)
-
+# Run the scheduled tasks indefinitely
 while True:
-    schedule.run_pending()
+    try:
+        schedule.run_pending()
+        time.sleep(1)
+    except Exception as e:
+        logging.error(f'Error during script execution: {str(e)}')
